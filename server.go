c0245aaf685dab0a6e04421ed4ed1389e8e37984
Ahmed Jafri <ajafri19@gmail.com>
2015-11-05 19:25:57 -0800

    Running go fmt on server.go to correct formatting.

--- server.go
+++ server.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 )
 
-
 func main() {
-  log.Println("Listening on server 3000")
-  
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/", fs)
-  http.ListenAndServe(":3000", nil)
+	log.Println("Listening on server 3000")
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.ListenAndServe(":3000", nil)
 }
